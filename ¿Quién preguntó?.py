45b0e942585efee486478a085cc42b228c13acb0
Hikiiro <108572737+Hikiiro@users.noreply.github.com>
2022-07-02 12:27:33 +0200

    Add files via upload

---
+++ ¿Quién preguntó?.py
@@ -0,0 +1,10 @@
+import time
+
+# En esta parte se intenta averiguar quién ha preguntado
+print("Buscando quién preguntó...\n")
+
+# Dejamos un tiempo de espera para procesar la información
+time.sleep(5)
+
+# Mostramos el resultado por pantalla
+print("Nadie\n")
