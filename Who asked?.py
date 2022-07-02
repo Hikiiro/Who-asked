89de57f161992b5f316be12e263b13ed50346fc8
Hikiiro <108572737+Hikiiro@users.noreply.github.com>
2022-07-02 12:27:06 +0200

    Add files via upload

---
+++ Who asked?.py
@@ -0,0 +1,10 @@
+import time
+
+# At this point the program is trying to find out who asked
+print("Looking for who asked...\n")
+
+# The program will take a moment to process the information
+time.sleep(5)
+
+# Showing the result on the screen
+print("Nobody\n")
